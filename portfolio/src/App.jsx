50b9947d2adf4a13bc7abe8204e3f300a671c5d7
emmanarthur4488 <arthurgodswill56@gmail.com>
2024-01-23 04:22:18 -0800

    first commit

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -4,15 +4,14 @@ import Navbar from './Components/Navbar/Navbar'
 import Home from './Components/Home/Home'
 import About from './Components/About/About'
 import Box from './Components/Box/Box'
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-  BrowserRouter,
-} from "react-router-dom";
 import Blog from './Components/Blog/Blog'
 import Contact from './Components/Contact/Contact'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
 
@@ -21,12 +20,12 @@ function App() {
       <BrowserRouter>
         <Navbar/>
 
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/blog' element={<Blog/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/blog' element={<Blog/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+        </Routes>
       </BrowserRouter>
     </>
   )
